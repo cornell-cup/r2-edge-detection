6af7fdbad0784e73df959bdc4ff3b3828363cc51
Alison Duan <aduan@dhcp-hol-4074.redrover.cornell.edu>
2019-11-09 16:17:32 -0500

    began shortest distance through the midpoint optimization

--- edgedetect.py
+++ edgedetect.py
@@ -1,5 +1,5 @@
 import time
-from builtins import int, len
+from builtins import int, len, range, list
 from PIL import Image
 import sys
 import numpy as np
@@ -26,14 +26,31 @@ def crop(x1, y1, x2, y2, image):
 #  parameters are the image file, the midpoint, and canny edge
 #  thresholds
 def canny_edge(image_file, t1, t2):
-    src = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
+    src = cv2.imread(image_file, cv2.IMREAD_GRAYSCALE)
 
+    #  TODO: Like the threshold values for canny edge, we need
+    #   to determine the kernel values for Gaussian blur
     #  apply Gaussian blur on src image
     blurred = cv2.GaussianBlur(src, (5, 5), cv2.BORDER_DEFAULT)
 
-    #  apply canny edge detection to the image
+    #  apply canny edge detection to the blurred image
     edge = cv2.Canny(blurred, t1, t2)
     return edge
+
+
+#  uses the canny edge image and the midpoint to determine the two points
+#  that the robot arm needs to grab
+def shortest_path(edge, mid, w, h):
+    pix_val = []
+    #  range goes from halfway through the x direction and
+    #  the whole way in the y direction
+    for i in range(mid[0] - w, mid[0]):
+        for j in range(mid[1] - h, mid[1] + h):
+            r, g, b = edge.getpixel(i, j)
+            if r == g == b == 0:
+                pix_val.append((i, j))
+
+    return 0
 
 
 #  assumes sys.argv[1] (first argument after the python script call)
@@ -46,7 +63,9 @@ def main():
         width, height = image.size
     #  TODO: Write code with the object detection script to return bounding box coordinates
     #   for now, just assume that the bounding box is the whole image, and no cropping is necessary.
-    #   Also need to determine what threshold values we need to use
+    #   Also need to determine what threshold values we need to use through adjustment, not hard code
     edge_image = canny_edge(image_file, 25, 55)
     edge_image.show()
     time.sleep(5)
+    mid = midpoint(0, 0, width, height)
+    print(shortest_path(edge_image, mid, int(width/2), int(height/2)))
